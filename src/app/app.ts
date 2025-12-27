ba72379b03388c5df4e970a142a064ff7cc8fe7b
aananev <6167006@bk.ru>
2025-12-27 15:17:48 +0300

    `1`

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+    selector: 'app-root',
+    imports: [],
+    template: `
+        <h1 class="bg-red-50 text-3xl font-bold underline">Hello world!</h1>
+    `,
+    styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('nkl-ecommerce');
+    protected readonly title = signal('nkl-ecommerce');
 }
